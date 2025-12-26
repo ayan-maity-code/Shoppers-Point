72047bb6c2cf0087e10010a6e3c10f121235be60
Ayan Maity <ayanmaity943@gmail.com>
2025-12-27 01:38:36 +0530

    First Commit

---
+++ src/main/java/com/shopperspoint/ShoppersPointApplication.java
@@ -0,0 +1,13 @@
+package com.shopperspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppersPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppersPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopperspoint/ShoppersPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopperspoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppersPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
